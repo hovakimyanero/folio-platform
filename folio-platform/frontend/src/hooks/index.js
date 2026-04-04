7be989a897336d1563041f643e921ddc878b60f3
Erik <102915922+hovakimyanero@users.noreply.github.com>
2026-04-05 01:35:28 +0300

    Delete folio-platform directory

--- folio-platform/frontend/src/hooks/index.js
+++
@@ -1,6 +0,0 @@
-export { useAuth } from './useAuth';
-export { useFetch } from './useFetch';
-export { useAsync } from './useAsync';
-export { useLocalStorage } from './useLocalStorage';
-export { useDebounce } from './useDebounce';
-export { useThrottle } from './useThrottle';
